1b360212cac89b9adc73d20d7704f1573b4299f5
zhahirasalma <zhahira.s.r@mail.ugm.ac.id>
2019-10-15 13:42:43 +0700

    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zhahira Salma Rafida
                 </div>
 
                 <div class="links">
